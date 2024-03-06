2218db5ef5a2e508139542e7c7145e0d0ad9790e
winston madiano <wmadiano@gmail.com>
2024-03-07 00:49:22 +0800

    using cluster IP

--- src/component/config/config.js
+++ src/component/config/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://api.gresdev.com';// process.env.REACT_APP_API_BASE_URL; //|| 'http://api.gresdev.com'; // Replace with your default URL
+const API_BASE_URL = 'http://10.108.83.129:80';//'http://api.gresdev.com';// process.env.REACT_APP_API_BASE_URL; //|| 'http://api.gresdev.com'; // Replace with your default URL
 const config = {
   apiBaseUrl: API_BASE_URL,
 };
